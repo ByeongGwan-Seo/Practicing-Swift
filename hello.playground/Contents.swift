8ee5d9962cfb52a0f56616f79183dc0ecc5caa7f
Byeong Gwan Seo <seo.jp.work@gmail.com>
2022-07-25 03:12:41 +0900

    함수와 구조체
    질문 사항은 Question. 정리해서 다음 연수 시간때 물어볼 것.

--- hello.playground/Contents.swift
+++ hello.playground/Contents.swift
@@ -224,3 +224,200 @@ print(name) //nil
 
 //이 타입이 실무에서 어떻게 사용되는지는 직접 프로젝트를 진행하던가 해야 알 수 있을 것 같다.
 
+//함수, 클로저 (function, closure)
+/*
+ 함수의 정의와 호출
+ func 함수명(파라미터: 형) -> 리턴타입 {
+ 
+ }
+ 
+ 파라미터를 받을 수도 안 받을수도, 리턴이 있을수도 없을수도 있으니 상황에 맞게 사용할 것.
+ 
+ */
+
+func printGugu(dan: Int) {
+    for i in 1...9 {
+        print("\(dan) * \(i) = \(dan * i)")
+    }
+}
+
+printGugu(dan: 5)
+
+func rollDice () -> Int {
+    return Int.random(in: 1...6)
+}
+
+let random = rollDice()
+
+print(random)
+
+/*
+ 클로저는 이름이 존재하지 않는 함수? 뭔 소리지...?
+ */
+
+//함수
+func call(name: String) {
+    print("hello, \(name)")
+}
+
+call(name: "Gom")
+
+// 상수에 함수를 할당하고 해당 상수를 호출
+let callName = call
+callName("Serker")
+
+// 상수에 클로저를 할당하고, 해당 상수 호출
+
+let callName2 = { (name: String) in
+    print("hi, \(name)")
+}
+
+callName2("lalaion")
+
+//in으로 파라미터 및 리턴 타입과 실제 클로저 코드를 분리한다.
+//배열, 딕셔너리 같은 컬렉션 타입(array, dictionary, set)과 함께 filter, map, reduce 메소드 사용시 자주 활용된다. 아래는 예시
+
+//filter
+let members = ["botan", "marin", "gura"]
+let nameStartsWithM = members.filter { name in
+    return name.hasPrefix("m")
+}
+
+print(nameStartsWithM)
+
+//map
+let prices = [1000, 2000, 3000]
+let doubledPrices = prices.map { price in
+    return price * 2
+}
+
+print(doubledPrices)
+
+//reduce
+let revenues = [100, 200, 300]
+let totalRevenue = revenues.reduce(0) {partialResult, next in
+    return partialResult + next
+}
+
+print(totalRevenue)
+
+
+/*
+ 구조체
+ 사용자가 원하는 데이터타입을 만들때 사용한다.
+ 인스턴스의 값(프로퍼티)을 저장하거나 기능(메소드)를 제공하고 이를 "캡슐화"할 수 있는, 스위프트에서 제공하는 타입
+ struct 키워드를 이용해서 선언한다.
+ */
+
+struct Album {
+    //Member variables
+    //stored property
+    
+    let title: String
+    let artist: String
+    var isReleased = false
+    
+    func description() -> String {
+        return "\(title) by \(artist)"
+    }
+    
+    //구조체 내부 멤버 변수의 값을 변경하는 경우, mutating 키워드를 이용한다.
+    //여기서는 isReleased가 상수가 아닌 변수이므로 mutating 키워드를 이용해 value를 변경한다.
+    
+    mutating func release() {
+        self.isReleased = true
+    }
+}
+
+var reWind = Album(title: "Re:wind", artist: "Isedol")
+print(reWind.description())
+
+print(reWind.isReleased)
+reWind.release()
+print(reWind.isReleased)
+print("===================")
+/*
+ 1. struct. 구조체를 통해 앨범의 멤버 변수(노래 제목, 가수)의 타입을 String으로 정한다.
+ 2. 구조체 내부에 상세 설명을 표시하는 함수 description을 선언하고 title by artist를 String으로 리턴한다.
+ 3. 앨범 구조체 내부에는 제목과 가수가 상수로, 발매 여부를 묻는 isRelease라는 변수가 선언되어 있다.
+ 4. isRelease의 default 값은 false로 선언되어 있는데, 이를 변경하는 함수를 작성하려면 mutating 키워드를 사용한다.
+ 5. mutating func release()의 경우, isReleased를 true로 변경하는 기능을 제공한다.
+ 
+ Question. self란 구조체 블록 내부에서 자기 자신을 가리킬때 사용하는 키워드인가? 자바의 this 같은...?
+ 
+ 6. reWind라는 변수는 앨범 구조체에 타이틀과 가수 파라미터를 받은 다음 아래행에서 상세 정보를 출력한다.
+ 7. 그리고 mutating func　선언을 한 relase함수를 호출해 isRelease의 밸류를 false에서 true로 변경한다.
+ 
+ Question. 내 자신이 이해한 흐름이 이게 맞는건가...?
+ */
+
+/*
+ 클래스
+ 사용자가 원하는 데이터타입을 만들때 사용한다는 점에서 구조체와 유사. 하지만 몇 가지 차이점이 있다.
+ 
+ 1. 클래스는 상속이 가능하다. 구조체는 불가능하다.
+ 2. 클래스는 참조(reference)하고 구조체는 복사(copy)한다.
+ 3. 클래스는 생성자(initializer)를 기본으로 만들어주지 않는다. (클래스 또는 구조첼ㄹ 생성할 때 사용하는 특별한 함수로, init 키워드로 선언한다.)
+ */
+
+class Employee {
+    var name: String
+    var hours: Int
+    
+    //생성자
+    init(name: String, hours: Int) {
+        self.name = name
+        self.hours = hours
+    }
+    
+    func work() {
+        print("I'm working now...")
+    }
+    
+    func summary() {
+        print("I work \(self.hours) hours a day")
+    }
+}
+
+//클래스 상속
+class iOSDeveloper: Employee {
+    
+    override func work() {
+        print("I'm developing iOS app now")
+    }
+    
+    override func summary() {
+        print("I work \(self.hours/2) hours a day")
+    }
+}
+
+struct Phone {
+    var modelName: String
+    var manufacturer: String
+    var version: Double = 1.0
+}
+
+let normalWorker = Employee(name: "gom", hours: 8)
+normalWorker.work()
+normalWorker.summary()
+
+let developer = iOSDeveloper(name: "serker", hours: 8)
+developer.work()
+developer.summary()
+
+print("---------------------")
+//reference vs. copy
+var iPhone1 = Phone(modelName: "iPhone 13", manufacturer: "Apple")
+var iPhone2 = iPhone1
+iPhone2.modelName = "iPhone 14"
+
+print(iPhone2.modelName)
+print(iPhone1.modelName)
+print("---------------------")
+
+var jrDeveloper1 = iOSDeveloper(name: "john", hours: 8)
+var jrDeveloper2 = jrDeveloper1
+jrDeveloper1.name = "Kenny"
+print(jrDeveloper1.name)
+print(jrDeveloper2.name)
+print("---------------------")
