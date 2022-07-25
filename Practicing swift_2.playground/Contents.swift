c0daa1fddc2dd540c4f68609ac1a79b3fc31097f
Byeong Gwan Seo <seo.jp.work@gmail.com>
2022-07-25 21:47:06 +0900

    Protocol
    이게 뭔데요 대체;;

--- Practicing swift_2.playground/Contents.swift
+++ Practicing swift_2.playground/Contents.swift
@@ -37,3 +37,37 @@ print(human.fullName)
 print("---------------------")
 
 //Question. model, manufacturer, firstName, lastName이 stored property. description, fullName이 computed property가 되는 건가?
+
+/*
+ 프로토콜(protocol)
+ 
+ 제공하고 싶은 역할(기능, 속성)을 미리 정의해 놓은 것
+ 이후에 다른 타입이 해당 프로토콜의 역할을 제공하고 싶으면 conform 해서 제공한다.
+ Question. 자바의 인터페이스 같은 건가? 클래스 상속과 프로토콜에는 무슨 차이가 있는가?
+ */
+
+protocol Coach {
+    var name: String {get set}
+    var currentTeam: String{get}
+    func training()
+    func direct()
+}
+
+struct Mourinho: Coach {
+    var name: String = "Jose Mourinho"
+    var currentTeam: String = "AS Roma"
+    
+    func training() {
+        print("Training Player...")
+    }
+    
+    func direct() {
+        print("Direct Game")
+    }
+}
+
+let mourinho = Mourinho()
+print("\(mourinho.name), \(mourinho.currentTeam)")
+mourinho.training()
+mourinho.direct()
+print("---------------------")
