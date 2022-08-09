09cf338fe1989d8f1c76d18c1ff56ff5fbb703b0
Byeong Gwan Seo <seo.jp.work@gmail.com>
2022-08-09 21:24:32 +0900

    PageControl complete
    그래도 얘는 그렇게 안어렵ㄷ...

---
+++ NRCOnboarding/NRCOnboarding/OnboardingCell.swift
@@ -0,0 +1,20 @@
+//
+//  OnboardingCell.swift
+//  NRCOnboarding
+//
+//  Created by seobyeonggwan on 2022/08/09.
+//
+
+import UIKit
+
+class OnboardingCell: UICollectionViewCell {
+    @IBOutlet weak var thumbnailImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    func configure(_ message: OnboardingMessage) {
+        thumbnailImageView.image = UIImage(named: message.imageName)
+        titleLabel.text = message.title
+        descriptionLabel.text = message.description
+    }
+}
--- NRCOnboarding/NRCOnboarding/OnboardingViewController.swift
+++ NRCOnboarding/NRCOnboarding/OnboardingViewController.swift
@@ -9,21 +9,69 @@ import UIKit
 
 class OnboardingViewController: UIViewController {
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var pageControl: UIPageControl!
+    
+    let messages: [OnboardingMessage] = OnboardingMessage.messages
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        
+        if let layout = collectionView.collectionViewLayout as? UICollectionViewFlowLayout {
+            layout.estimatedItemSize = .zero
+        }
+        
+        pageControl.numberOfPages = messages.count
+    }
+}
 
-        // Do any additional setup after loading the view.
+extension OnboardingViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return messages.count
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "OnboardingCell", for: indexPath) as? OnboardingCell else {
+            return UICollectionViewCell()
+        }
+        let message = messages[indexPath.item]
+        cell.configure(message)
+        return cell
     }
-    */
+}
 
+extension OnboardingViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return collectionView.bounds.size
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return .zero
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return .zero
+    }
+}
+
+extension OnboardingViewController: UIScrollViewDelegate {
+//    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+//        print(Int(scrollView.contentOffset.x / self.collectionView.bounds.width))
+//    }
+    
+//    func scrollViewWillBeginDecelerating(_ scrollView: UIScrollView) {
+//        print("it started")
+//    }
+//
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        print("it stopped")
+        
+        let index = Int(scrollView.contentOffset.x / self.collectionView.bounds.width)
+        
+        pageControl.currentPage = index
+    }
 }
