a172b0ee53a27253ddfab97570b6686301198f58
Byeong Gwan Seo <seo.jp.work@gmail.com>
2022-08-18 13:44:08 +0900

    Applied QuickFocusUI constraints

---
+++ HeadSpaceFocus_navigation/HeadSpaceFocus/QuickFocusListViewController.swift
@@ -0,0 +1,18 @@
+//
+//  QuicFocusListViewController.swift
+//  HeadSpaceFocus
+//
+//  Created by seobyeonggwan on 2022/08/18.
+//
+
+import UIKit
+
+class QuickFocusListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    //2개의 section, section안에 5개의 group, group안에 3개의 item(image, Label, Label)
+}
