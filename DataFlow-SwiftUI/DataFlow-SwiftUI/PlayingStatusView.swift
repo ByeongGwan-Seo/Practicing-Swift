86f0d09afc23e75e287f2a7f9ab98fa437fd3e32
Byeong Gwan Seo <seo.jp.work@gmail.com>
2022-08-22 16:18:33 +0900

    handling data with Swift UI

---
+++ DataFlow-SwiftUI/DataFlow-SwiftUI/PlayingStatusView.swift
@@ -0,0 +1,22 @@
+//
+//  PlayingStatusView.swift
+//  DataFlow-SwiftUI
+//
+//  Created by seobyeonggwan on 2022/08/22.
+//
+
+import SwiftUI
+
+struct PlayingStatusView: View {
+    
+    @Binding var isPlaying: Bool
+    
+    var body: some View {
+        Image(systemName: isPlaying ? "sun.max.fill" : "sun.max")
+            .resizable()
+            .renderingMode(.original)
+            .aspectRatio(contentMode: .fit)
+            .frame(width: 80, height: 80)
+    }
+}
+
